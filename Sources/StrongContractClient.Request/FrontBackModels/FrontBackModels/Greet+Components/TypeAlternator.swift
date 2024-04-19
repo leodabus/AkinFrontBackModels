d7ad51087ff3c13f95501fe8f1351e1be5f7a32b
scott-lydon <scottlydon18@gmail.com>
2024-04-19 00:45:36 -0700

    Make more structs public.

--- Sources/StrongContractClient.Request/FrontBackModels/FrontBackModels/Greet+Components/TypeAlternator.swift
+++ Sources/StrongContractClient.Request/FrontBackModels/FrontBackModels/Greet+Components/TypeAlternator.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum TypeAlternator<Preferred: Codable, Secondary: Codable>: Codable {
+public enum TypeAlternator<Preferred: Codable, Secondary: Codable>: Codable {
 
     case preferred(Preferred)
     case secondary(Secondary)
