93e8f58fd4a883382db8999e9e17531d2fe15217
Leonardo Savio Dabus <leodabus@gmail.com>
2024-05-09 11:18:43 -0300

    bump strong contract client to 6.0.2

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Existing EncryptDecryptKey package dependency
         // Add StrongContractClient package dependency
-        .package(url: "https://github.com/leodabus/StrongContractClient.git", from: "6.0.1"),
+        .package(url: "https://github.com/leodabus/StrongContractClient.git", from: "6.0.2"),
     ],
     targets: [
         .target(
